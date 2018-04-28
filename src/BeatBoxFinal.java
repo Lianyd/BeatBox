d77b9ffd94049a161a25e907f858070115d642e0
liany <601292628@qq.com>
2018-04-28 16:58:50 +0800

    0

--- src/BeatBoxFinal.java
+++ src/BeatBoxFinal.java
@@ -14,7 +14,7 @@ public class BeatBoxFinal {
     JPanel mainPanel;
     JList incomingList;
     JTextField userMessage;
-    ArrayList<JCheckBox> checkboxList;  // 把checkbok存储在arraylist中
+    ArrayList<JCheckBox> checkboxList;
     int nextNum;
     Vector<String> listVector = new Vector<String>();
     String userName;
@@ -36,38 +36,38 @@ public class BeatBoxFinal {
     int[] instruments = {35,42,46,38,49,39,50,60,70,72,64,56,58,47,67,63};   // 不同乐器的关键字
 
     public static void main (String[] args){
-        new BeatBoxFinal().startUp(args[0]);   // args[0] is your user ID/screen name
+        new BeatBoxFinal().startUp("I'm BOB");   // args[0] is your user ID/screen name
     }
 
     public void startUp(String name){
         userName = name;
         // open connection to the server
         try{
-            Socket sock = new Socket("127.0.0.1",4242);
-            out = new ObjectOutputStream(sock.getOutputStream());
-            in = new ObjectInputStream(sock.getInputStream());          //设置网络、输入/输出，并创建出 reader 的线程
-            Thread remote = new Thread(new RemoteReader());
+            Socket sock = new Socket("127.0.0.1",4242);                            // 连接服务器IP（此处为本地IP），以及TCP端口
+            out = new ObjectOutputStream(sock.getOutputStream()); //写给服务器的对象信息流
+            in = new ObjectInputStream(sock.getInputStream());    //从服务器读取的对象信息流     //设置网络、输入/输出，并创建出 reader 的线程
+            Thread remote = new Thread(new RemoteReader());       // 将读取信息功能放入新的线程中，具体实现方法在remotereader中
             remote.start();
         }catch (Exception ex){
             System.out.println("couldn't connect - you'll hava to play alone.");
         }
-        setUpMidi();
-        buildGUI();
+        setUpMidi();  // 创建播放器、CD、专辑、播放速度等
+        buildGUI();   // 绘制主界面
     }
 
     public void buildGUI(){
 
         theFrame = new JFrame("Cyber BeatBoxFinal");
-        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 关闭界面后停止运行程序
         BorderLayout layout = new BorderLayout();
-        JPanel background = new JPanel(layout); // 将background面板设置为layout布局
+        JPanel background = new JPanel(layout); // 将background面板设置为layout布局,否则JPanel布局为flowlayout形式
         background.setBorder(BorderFactory.createEmptyBorder(10,10,10,10)); // 设定面板上摆设组件时的空白边缘
 
-        checkboxList = new ArrayList<JCheckBox>();
+        checkboxList = new ArrayList<JCheckBox>();  // 用来存储每个方块的勾选情况
 
-        Box buttonBox = new Box(BoxLayout.Y_AXIS);
-        JButton start = new JButton("start");
-        start.addActionListener(new MyStartListener());
+        Box buttonBox = new Box(BoxLayout.Y_AXIS);  // 用来存放交互按钮的box
+        JButton start = new JButton("start");  // start按钮
+        start.addActionListener(new MyStartListener());  // 按钮监听功能
         buttonBox.add(start);
 
         JButton stop = new JButton("stop");
@@ -82,13 +82,15 @@ public class BeatBoxFinal {
         downTempo.addActionListener(new MyDownTempoListener());
         buttonBox.add(downTempo);
 
-//        JButton Serializelt = new JButton("save");
+        JButton save = new JButton("save");
 //        Serializelt.addActionListener(new MySendListener());
 //        buttonBox.add(Serializelt);
-//
-//        JButton Restore = new JButton("load");
+        buttonBox.add(save);
+
+        JButton load = new JButton("load");
 //        Restore.addActionListener(new MyReadInListener());
 //        buttonBox.add(Restore);
+        buttonBox.add(load);
 
         JButton sendIt = new JButton("sendIt");
         sendIt.addActionListener(new MySendListener());
@@ -115,12 +117,12 @@ public class BeatBoxFinal {
         theFrame.getContentPane().add(background);
 
         GridLayout grid = new GridLayout(16,16);
-        grid.setVgap(1);
-        grid.setHgap(2);
+        grid.setVgap(1); // 网格垂直相邻距离
+        grid.setHgap(2); // 网格水平相邻距离
         mainPanel = new JPanel(grid);
         background.add(BorderLayout.CENTER,mainPanel);
 
-        for (int i = 0; i < 256; i++){     // 创建checkbox组，设定未勾选的为false，加到arraylist和面板上
+        for (int i = 0; i < 256; i++){     //  设定未勾选的方框为false，则已勾选的为true，将扫描信息存在ckeckboxList中，并且发给面板
             JCheckBox c = new JCheckBox();
             c.setSelected(false);
             checkboxList.add(c);
@@ -128,7 +130,7 @@ public class BeatBoxFinal {
         }
 
         theFrame.setBounds(50,50,300,300);
-        theFrame.pack();
+        theFrame.pack(); // 使此窗口的大小适合其子组件的首选大小和布局
         theFrame.setVisible(true);
     }
 
@@ -159,7 +161,7 @@ public class BeatBoxFinal {
                     trackList.add(null);
                 }
             }
-            makeTracks(trackList);
+            makeTracks(trackList); // 内循环，某种乐器的16拍，外循环，切换16种乐器；tracklist分别的到每种乐器的16拍信息后用maketracks制作需要播放的专辑
         }
 
         track.add(makeEvent(192,9,1,0,15));// 确保第16拍有事件，否则beatbox不会重复播放
@@ -171,7 +173,7 @@ public class BeatBoxFinal {
         }catch (Exception e){e.printStackTrace();}
     }
 
-    // 6个监听的内部类
+    // 多个监听的内部类
     public class MyStartListener implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent a){
@@ -213,7 +215,7 @@ public class BeatBoxFinal {
                 }
             }
             try{
-                out.writeObject(userName + nextNum++ + ":" + userMessage.getText());
+                out.writeObject(userName + nextNum++ + ":" + userMessage);
                 out.writeObject(checkboxState);
             }catch (Exception ex){
                 System.out.println("Sorry dude. Could not send it to the server.");
@@ -303,7 +305,7 @@ public class BeatBoxFinal {
         for (int i = 0; i < 16; i++){
             Integer num = (Integer)it.next();
             if (num != null){
-                int numKey = num.intValue();
+                int numKey = num.intValue();  // 将 Integer的值作为 int
                 track.add(makeEvent(144,9,numKey,100,i));
                 track.add(makeEvent(128,9,numKey,100,i+1));
             }
